b789a4e74d278fd518c7d127b709add7783aaa0f
arif-projects <arifulhaque60332@gmail.com>
2023-03-01 09:14:24 +0600

    firstCommit

---
+++ deafultParameter.js
@@ -0,0 +1,8 @@
+function add(num1 = 0, num2 = 0) {
+  const sum = num1 + num2;
+  return sum;
+}
+
+const result = add(20);
+//result will be 20
+console.log(result);
---
+++ distructure.js
@@ -0,0 +1,18 @@
+//object distructure
+
+const fish = {
+  name: "hilsha",
+  address: "Chadpur",
+  price: 4000,
+  color: "Silver",
+};
+
+const { name, color, price } = fish;
+
+console.log(price, name);
+
+// array distructure
+
+const [first, second] = [40, 60];
+
+console.log(second);
